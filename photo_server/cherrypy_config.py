79947a5b12dac6a25f90a77d717e70d22baacb5f
root <root@li239-22.members.linode.com>
2011-02-07 03:19:39 +0000

    "bunch of html stuff"

--- photo_server/cherrypy_config.py
+++ photo_server/cherrypy_config.py
@@ -1,2 +1,25 @@
 [global]
 server.socket_host = "0.0.0.0"
+
+[/]
+tools.staticdir.root = "/var/www2/facebook/html/"
+
+[/client]
+tools.staticfile.on = True
+tools.staticfile.filename = "/var/www2/facebook/html/index.html"
+
+[/lol.html]
+tools.staticfile.on = True
+tools.staticfile.filename = "/var/www2/facebook/html/lol.html"
+
+[/img/bg.jpg]
+tools.staticfile.on = True
+tools.staticfile.filename = "/var/www2/facebook/html/img/bg.jpg"
+
+[/js/main.js]
+tools.staticfile.on = True
+tools.staticfile.filename = "/var/www2/facebook/html/js/main.js"
+
+[/css/main.css]
+tools.staticfile.on = True
+tools.staticfile.filename = "/var/www2/facebook/html/css/main.css"
--- photo_server/memeserver.py
+++ photo_server/memeserver.py
@@ -3,9 +3,10 @@ import cherrypy
 class HelloWorld:
     def index(self, url = None, top = None, bot = None, x = None, y = None, width = None, height = None):
         if not url:
-            return "no url"
+            return file('../html/index.html').read()
         cherrypy.response.headers["Content-Type"] = "image/jpeg"
         return memeify(url, top, bot, x, y, width, height)
     index.exposed = True
 
 cherrypy.quickstart(HelloWorld(), "/", "cherrypy_config.py")
+
